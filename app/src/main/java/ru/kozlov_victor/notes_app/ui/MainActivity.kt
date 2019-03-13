2e70058102a4efa4b0065f94353d66f298236bf7
Admin <hostelv@list.ru>
2019-03-13 16:10:22 +0600

    add fragments

--- app/src/main/java/ru/kozlov_victor/notes_app/ui/MainActivity.kt
+++ app/src/main/java/ru/kozlov_victor/notes_app/ui/MainActivity.kt
@@ -1,12 +1,15 @@
-package ru.kozlov_victor.notes_app
+package ru.kozlov_victor.notes_app.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ru.kozlov_victor.notes_app.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_frame, MainFragment())
     }
+
 }
---
+++ app/src/main/java/ru/kozlov_victor/notes_app/ui/MainFragment.kt
@@ -0,0 +1,10 @@
+package ru.kozlov_victor.notes_app.ui
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class MainFragment : Fragment() {
+}
