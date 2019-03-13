789920c22717a8f38c05759e2d8dcc291a8c9334
Admin <hostelv@list.ru>
2019-03-13 16:36:58 +0600

    add some change in main fragment

--- app/src/main/java/ru/kozlov_victor/notes_app/ui/MainActivity.kt
+++ app/src/main/java/ru/kozlov_victor/notes_app/ui/MainActivity.kt
@@ -9,7 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction().replace(R.id.fragment_frame, MainFragment())
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_frame, MainFragment())
+            .commit()
     }
 
 }
--- app/src/main/java/ru/kozlov_victor/notes_app/ui/MainFragment.kt
+++ app/src/main/java/ru/kozlov_victor/notes_app/ui/MainFragment.kt
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ru.kozlov_victor.notes_app.R
 
 class MainFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
 }
